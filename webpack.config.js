c65520c875d7f5c51ca9d4c6f2bf63aeb715b922
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2023-09-18 17:20:44 +0700

    initialize webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Your main JavaScript file
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), // Output directory
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
